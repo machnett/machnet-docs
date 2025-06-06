e6ac42305b239c442b48884f5d4cb5c9bafb55f6
Alireza Sanaee <sarsanaee@gmail.com>
2025-06-06 17:07:40 +0100

    a few updates

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -15,17 +15,18 @@ const config = {
   favicon: 'img/favicon.ico',
 
   // Set the production url of your site here
-  url: 'https://machnet.io',
+  url: 'https://machnett.github.io',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/',
+  baseUrl: '/machnet-docs/',
 
+  deploymentBranch: 'main',   // Or your preferred branch
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
   organizationName: 'facebook', // Usually your GitHub org/user name.
   projectName: 'machnet', // Usually your repo name.
 
-  onBrokenLinks: 'throw',
+  onBrokenLinks: 'warn',
   onBrokenMarkdownLinks: 'warn',
 
   // Even if you don't use internationalization, you can use this field to set
